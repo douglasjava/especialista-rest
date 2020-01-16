f524c7e031b9ddc427e92e61972295ee79cd44a6
Douglas Dias <douglasmarquesdias@hotmail.com>
2020-01-15 23:05:57 -0300

    inclusão de dois enpoints

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -3,12 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +57,29 @@ public class RestauranteController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 
 		}
+	}
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+
+		try {
+			
+			Optional<Restaurante> restauranteAtualOptional = restauranteRepository.findById(restauranteId);
+			
+			if (restauranteAtualOptional.isPresent()) {
+				Restaurante restauranteAtual = restauranteAtualOptional.get();
+				
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+				
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
 	}
 }
