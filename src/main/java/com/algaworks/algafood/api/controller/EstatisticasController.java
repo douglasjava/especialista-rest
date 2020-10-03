ac4e0265d3137ee26256a292c7359e63a646dd07
Douglas Dias <douglasmarquesdias@hotmail.com>
2020-10-03 15:07:02 -0300

    criação de relatorio pdf

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,6 +3,11 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
+import com.algaworks.algafood.infrastructure.repository.service.report.VendaReportServiceImpl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +20,35 @@ import java.util.List;
 public class EstatisticasController {
 
     private VendaQueryService vendaQueryService;
+    private VendaReportService vendaReportService;
 
-    public EstatisticasController(VendaQueryService vendaQueryService) {
+    public EstatisticasController(VendaQueryService vendaQueryService, VendaReportService vendaReportService) {
         this.vendaQueryService = vendaQueryService;
+        this.vendaReportService = vendaReportService;
     }
 
-    @GetMapping("/vendas-diarias")
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter vendaDiariaFilter,
-                        @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+                                                    @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
         return vendaQueryService.consultarVendaDiarias(vendaDiariaFilter, timeOffset);
+    }
+
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter vendaDiariaFilter,
+                                                    @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+
+        byte[] bytesPdf = vendaReportService.emitirVendasDiarias(vendaDiariaFilter, timeOffset);
+
+        //Usar a propriedade "attachment" quando for necessário fazer o download e não ser exibido.
+
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=vendas-diarias.pdf");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .headers(headers)
+                .body(bytesPdf);
     }
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/report/VendaReportServiceImpl.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.infrastructure.repository.service.report;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+@Service
+public class VendaReportServiceImpl implements VendaReportService {
+
+    private VendaQueryService vendaQueryService;
+
+    public VendaReportServiceImpl(VendaQueryService vendaQueryService) {
+        this.vendaQueryService = vendaQueryService;
+    }
+
+    @Override
+    public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
+        try {
+            var inputStream = this.getClass().getResourceAsStream(
+                    "/relatorios/vendas-diarias.jasper");
+
+            var parametros = new HashMap<String, Object>();
+            parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            var vendasDiarias = vendaQueryService.consultarVendaDiarias(filtro, timeOffset);
+            var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+            var jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new ReportException("Não foi possível emitir relatório de vendas diárias", e);
+        }
+    }
+
+}
